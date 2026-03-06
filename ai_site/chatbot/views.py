314c60f0bb9ed3507eb7a176cebf25c49bc91a39
unknown <prajjurajkumar92@gmail.com>
2026-03-06 12:43:22 +0530

    activate

--- ai_site/chatbot/views.py
+++ ai_site/chatbot/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from google import genai
+from django.conf import settings
 
-# Create your views here.
+client = genai.Client(api_key=settings.GEMINI_API_KEY)
+
+def home(request):
+    response_text = ""
+
+    if request.method == "POST":
+        question = request.POST.get("question")
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=question,
+        )
+
+        response_text = response.text
+
+    return render(request, "index.html", {"response": response_text})
